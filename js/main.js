335a8ff7af046a44c50e3a8a8895b15ad4231a91
wang0nya <kwangonya@gmail.com>
2018-05-01 13:31:42 +0300

    add loader

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 (function () {
 	"use strict";
+	// Wait for window load
+	$(window).on('load', function () {
+		// Animate loader off screen
+		$(".se-pre-con").fadeOut("slow");
+	});
 	// navbar scroll effect
 	$(window).scroll(function () {
 		if ($(window).scrollTop() >= 50) {
